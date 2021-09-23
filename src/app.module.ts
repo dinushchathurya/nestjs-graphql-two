6bc811df2e5643ae39056c5c66d5987dbaff19f0
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-23 21:48:19 +0530

    config: Peering ormconfig.ts & graphql schema build options

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import config from 'ormconfig';
+import { EmployeeModule } from './employee/employee.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    EmployeeModule,
+    GraphQLModule.forRoot({ 
+      autoSchemaFile: join(process.cwd(), 'src/graphql-schema.gql'),
+    }),
+    TypeOrmModule.forRoot(config)
+],
   controllers: [],
   providers: [],
 })
+
 export class AppModule {}
