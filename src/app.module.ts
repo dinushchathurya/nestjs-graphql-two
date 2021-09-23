46e9156ecc027f328c866384dfa9156de858ed4e
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-23 22:58:56 +0530

    config: Peering Project module in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import config from 'ormconfig';
 import { EmployeeModule } from './employee/employee.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectModule } from './project/project.module';
 
 
 @Module({
   imports: [
     EmployeeModule,
+    ProjectModule,
     GraphQLModule.forRoot({ 
       autoSchemaFile: join(process.cwd(), 'src/graphql-schema.gql'),
     }),
-    TypeOrmModule.forRoot(config)
-],
-  controllers: [],
-  providers: [],
+    TypeOrmModule.forRoot(config),
+  ]
 })
 
 export class AppModule {}
