469225bda77ffdbea41d0cabe9349002c2978626
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-24 08:12:46 +0530

    feat: Create peoject resource to handle project related function and services

---
+++ src/project/entities/project.entity.ts
@@ -0,0 +1,20 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, PrimaryGeneratedColumn, Entity } from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class Project {
+
+  @Field()
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Field()
+  @Column()
+  name: string
+
+  @Field(()=> Int)
+  @Column()
+  code: number
+  
+}
